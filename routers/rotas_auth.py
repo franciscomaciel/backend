c64b4fa877d81690c49002e9504cc4bb911a8f30
Francisco Maciel <francisco.maciel@gmail.com>
2021-10-22 07:29:52 -0300

    Utilizar método hash_provider.gerar_hash() para criptografar senhas dos usuários na criação daqueles.

--- routers/rotas_auth.py
+++ routers/rotas_auth.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, status, Depends, HTTPException
-from schemas.schemas import Usuario, UsuarioSimples
+from fastapi import APIRouter
 from starlette import status
 from ..infra.providers import hash_provider
 
+from schemas.schemas import Usuario, UsuarioSimples
 
 router = APIRouter()
 
@@ -10,5 +10,6 @@ router = APIRouter()
              status_code=status.HTTP_201_CREATED,
              response_model=UsuarioSimples)
 def signup(usuario: Usuario):
+    usuario.password = hash_provider.gerar_hash(usuario.password)
     usuario_criado = None   # TODO: Implementar
     return usuario_criado
