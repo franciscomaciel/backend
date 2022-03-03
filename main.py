6e900aa7c3a900ea107a400792adaa1dc00051b4
Francisco Maciel <francisco.maciel@gmail.com>
2022-03-02 22:45:28 -0300

    Desfazendo alteração no mecanismo de CORS.

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from routers import rotas_pedidos, rotas_auth
 # CORS
 origins = [
     "http://10.10.10.236:3000",
-    "http://10.10.10.236:8000", # TESTE
     "http://0.0.0.0:3000",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
